dddad43b4eacdf15aea48e6f4fbabc33fdb3f63a
Apollo1515 <eismc2rocks@gmail.com>
2022-04-21 11:03:47 -0700

    Added collider exceptions

--- Assets/Scripts/Player Scripts/Weapon.cs
+++ Assets/Scripts/Player Scripts/Weapon.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public float dmg;
-    private bool canDmg;
+    public bool canDmg;
     public float attackDuriation;
     [SerializeField] private float timeSinceAttack;
 
