9ff7b0f7043c2b805d37ba95beb2f9516a7260e6
Apollo1515 <eismc2rocks@gmail.com>
2022-04-26 07:33:36 -0700

    Added magic blast

---
+++ Assets/Scripts/Player Scripts/MagicSystem.cs
@@ -0,0 +1,64 @@
+using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicSystem : Weapon
+{
+    // Assign a Rigidbody component in the inspector to instantiate
+
+    ///public Rigidbody bulletPrefab;
+    public GameObject bulletPrefab;
+    bool Ready_to_attack = true;
+    public int explosion_size = 2;
+    GameObject clone;
+
+    void Update()
+    {
+        timeSinceAttack += Time.deltaTime;
+        if (timeSinceAttack > attackDuriation)
+        {
+            canDmg = false;
+        }
+        if (clone == null)
+        {
+            Ready_to_attack = true;
+        }
+
+        // Put this in your shoot function
+        //Instantiate(bulletPrefab, GUNTRANSFORM, GUNTRANSFORM.rotation, transform);
+        // Ctrl was pressed, launch a projectile
+        //if (Input.GetKeyDown(KeyCode.X))
+        //{
+        //Debug.Log("print x");
+        // Instantiate the projectile at the position and rotation of this transform
+
+
+
+        // Give the cloned object an initial velocity along the current
+        // object's Z axis
+        //clone.velocity = transform.TransformDirection(new Vector2(1,0) * 10);
+        //}
+    }
+
+    public override void Attack(PlayerMovement playerMovement)
+    {
+        if (Ready_to_attack)
+        {
+            clone = Instantiate(bulletPrefab, transform.position + Vector3.down * 1, transform.rotation);
+            if (!playerMovement.isTurned)
+                clone.transform.RotateAround(clone.transform.position, Vector3.forward, 180);
+            Weapon arrowWpn = clone.GetComponent<Weapon>();
+            arrowWpn.Attack(playerMovement);
+            canDmg = true;
+            timeSinceAttack = 0;
+            Ready_to_attack = false;
+        }
+        else
+        {
+            clone.transform.localScale += new Vector3(explosion_size, explosion_size, 0);
+            Destroy(clone);
+            Ready_to_attack = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerSword.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerSword : MonoBehaviour
+{
+    public float dmg;
+    public bool canDmg;
+    public float attackDuriation;
+    public GameObject sword_area;
+    [SerializeField] public float timeSinceAttack;
+
+    void Update()
+    {
+        Renderer rend = sword_area.transform.GetComponent<Renderer>();
+        Color matColor = rend.material.color;
+        float a_value = rend.material.color.a;
+        BoxCollider2D sword_Collider = sword_area.GetComponent<BoxCollider2D>();
+
+        timeSinceAttack += Time.deltaTime;
+        if (timeSinceAttack > attackDuriation)
+        {
+            canDmg = false;
+
+        }
+        if (!canDmg)
+        {
+            sword_Collider.size = new Vector2(0, 0);
+            rend.material.color = new Color(matColor.r, matColor.g, matColor.b, 0);
+        }
+        else
+        {
+            sword_Collider.size = new Vector2(1, 1);
+            rend.material.color = new Color(matColor.r, matColor.g, matColor.b, 0.7f);
+        }
+    }
+
+    public virtual void Attack(Platformer.Mechanics.PlayerMovement playerMovement)
+    {
+        Debug.Log("attacked");
+        canDmg = true;
+
+        Vector3 playerPos = gameObject.transform.position;
+        Vector3 playerDirection = gameObject.transform.forward;
+        Quaternion playerRotation = gameObject.transform.rotation;
+        float spawnDistance = 2;
+
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+
+        sword_area.transform.position =spawnPos;
+
+        timeSinceAttack = 0;
+    }
+
+    public void StopAttack()
+    {
+        canDmg = false;
+    }
+
+    public bool CanDamage() // Is the weapon's attack active?
+    {
+        return canDmg;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+
+    //   Debug.Log("collided");
+    //   if (canDmg && collision.CompareTag("Enemy") && !collision.GetComponent<Enemy>().invincible)
+    //   {
+    //       Debug.Log("damage");
+    //       ///gameObject bg = other.gameObject;
+
+    //       collision.gameObject.GetComponent<Enemy>().Destruct();
+    //       canDmg = false;
+
+    //    }
+
+    //}
+}
+
