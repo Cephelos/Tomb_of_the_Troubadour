da8af0bbd555cb3c46e28751948dc49c2b208356
Amber <amberyueliu@gmail.com>
2022-04-28 15:46:25 -0400

    typo fix

--- Assets/Scripts/Room Scripts/Door.cs
+++ Assets/Scripts/Room Scripts/Door.cs
@@ -25,6 +25,6 @@ public class Door : MonoBehaviour
         newRoom.gameObject.SetActive(true);
         Platformer.Mechanics.GameController.Instance.currentRoom = newRoom;
         newRoom.ResetRoom();
-         ebug.Log("here");
+        debug.Log("here");
     }
 }
