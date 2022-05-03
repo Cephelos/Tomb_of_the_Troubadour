5f719770a0f79777e0967f95626f074d2e0fa996
Apollo1515 <eismc2rocks@gmail.com>
2022-05-03 13:02:30 -0700

    Fixed Knockback

---
+++ Assets/Scripts/Player Scripts/EnemyCollision.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Platformer.Mechanics
+{
+
+public class EnemyCollision : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    private Transform playerTransform;
+    private Animator animator;
+    private Health health;
+
+    private PlayerMovement player;
+
+    private Vector3 direction;
+
+        private Weapon wep_script;
+
+        private GameObject weapon;
+
+    //public enum State { Attack, Move, Idle };
+
+    [SerializeField] private float knockbackX = 5;
+    [SerializeField] private float knockbackY = 5;
+    [SerializeField] private float stunTime= 0.50f;
+    [SerializeField] private float invincibleTime = 1f;
+    void Awake(){
+        rBody = GetComponent<Rigidbody2D>();
+        playerTransform = GetComponent<Transform>();
+        player = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+        health = GetComponent<Health>();
+        weapon = gameObject.transform.GetChild(2).gameObject;
+        wep_script = weapon.GetComponent<Weapon>();
+
+        }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            
+        /*if (collision.CompareTag("Enemy")  && (collision.gameObject.GetComponent<Skeleton>() == null || collision.gameObject.GetComponent<Skeleton>().isAttacking ) && !wep_script.canDmg && player.invincibleTimer == 0)
+        {
+            Debug.Log("tag" + collision.gameObject.tag);
+            Vector3 enemyDir = (playerTransform.position - collision.gameObject.transform.position).normalized;
+            if (enemyDir.x < 0)
+            {
+                knockbackX = -knockbackX;
+            }
+
+            rBody.velocity = Vector3.zero;
+            rBody.AddForce(new Vector3(knockbackX, knockbackY, 0), ForceMode2D.Impulse);
+
+            player.stunTimer = stunTime;
+            player.invincibleTimer = invincibleTime;
+
+            // Damage the player
+            health.Decrement();
+
+            // Tell animator that the player has been hurt
+            animator.SetBool("Hurt", true);
+
+                // Play SFX
+            GameController.Instance.audioController.PlaySFX("Player Hit");
+            
+        }*/
+        if (collision.CompareTag("Lava")) {
+            Debug.Log("Lava");
+            Destroy (gameObject);
+
+                // Play death SFX
+            GameController.Instance.audioController.PlaySFX("Player Die");
+        }
+
+    }
+}
+}
--- Assets/Scripts/Player Scripts/Health.cs
+++ Assets/Scripts/Player Scripts/Health.cs
@@ -19,6 +19,7 @@ public class Health : MonoBehaviour
         PlayerSettings playerSettings = gameObject.GetComponent<PlayerSettings>();
         currentHP = playerSettings.maxHP;
         maxHP = playerSettings.maxHP;
+        playerTransform = GetComponent<Transform>();
     }
 
     public void Increment(int incrementValue)
@@ -26,7 +27,7 @@ public class Health : MonoBehaviour
         currentHP = Mathf.Clamp(currentHP + incrementValue, 0, maxHP);
     }
 
-    public void Decrement(int decrementValue)
+    public void Decrement(int decrementValue, float knockbackX, float knockbackY, Vector3 enemy_pos)
     {
         Debug.Log("decreasing health");
         currentHP = Mathf.Clamp(currentHP - decrementValue, 0, maxHP);
@@ -43,6 +44,27 @@ public class Health : MonoBehaviour
         {
             Debug.Log("hurt animation");
             animator.SetBool("Stun", true);
+            
+            Vector3 enemyDir = (playerTransform.position - enemy_pos).normalized;
+            if (enemyDir.x < 0)
+            {
+                knockbackX = -knockbackX;
+            }
+
+            rBody.velocity = Vector3.zero;
+            rBody.AddForce(new Vector3(knockbackX, knockbackY, 0), ForceMode2D.Impulse);
+
+            player.stunTimer = stunTime;
+            player.invincibleTimer = invincibleTime;
+
+
+            // Tell animator that the player has been hurt
+            //    animator.SetBool("Hurt", true);
+
+            // Play SFX
+            GameController.Instance.audioController.PlaySFX("Player Hit");
+
+            
             Debug.Log("in stun animation state: " + animator.GetCurrentAnimatorStateInfo(0).IsName("stun"));
         }
     }
