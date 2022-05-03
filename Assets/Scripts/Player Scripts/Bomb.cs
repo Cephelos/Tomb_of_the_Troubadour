eb1fe84e7521cbc16acb2a7b4a05e21ca3a9cfd8
Apollo1515 <eismc2rocks@gmail.com>
2022-05-02 19:14:04 -0700

    Update Bomb.cs

--- Assets/Scripts/Player Scripts/Bomb.cs
+++ Assets/Scripts/Player Scripts/Bomb.cs
@@ -32,7 +32,7 @@ public class Bomb : Weapon
             if (enemy != null)
             {
                 Debug.Log("Hit enemy!");
-                enemy.Destruct();
+                enemy.Decrement();
             }
         }
 
